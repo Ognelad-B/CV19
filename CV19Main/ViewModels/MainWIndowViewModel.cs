a3e73eb653b143885a342d1bcf2723895734b663
Ognelad-B <70412189+Ognelad-B@users.noreply.github.com>
2020-11-19 00:07:24 +0200

    Связал заголовок окна с TextBox внутри окна

--- CV19Main/ViewModels/MainWIndowViewModel.cs
+++ CV19Main/ViewModels/MainWIndowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CV19Main.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,24 @@ using System.Threading.Tasks;
 
 namespace CV19Main.ViewModels
 {
-    class MainWIndowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        private string _Title = "Анализ статистики Covid-19";
+
+        /// <summary> Заголовок окна /// </summary>
+        public string Title
+        {
+            get => _Title;
+            //set =>
+                //if (Equals(_Title, value)) return;
+                //_Title = value;
+                //OnProperyChenged();
+                //Set(ref _Title, value);
+                //if (Equals(_Title, value)) return;
+                //_Title = value;
+                //OnProperyChenged();
+                //Set(ref _Title, value);
+            set => Set(ref _Title, value);
+        }
     }
 }
