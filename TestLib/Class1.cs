a65c58e2bd368afbca3637b88651de08783b05bc
Ognelad-B <70412189+Ognelad-B@users.noreply.github.com>
2020-11-21 22:53:20 +0200

    Создал 2 библиотеки

--- TestLib/Class1.cs
+++ TestLib/Class1.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Windows.Input;
 
 namespace TestLib
 {
-    public class Class1
+    internal abstract class Command : ICommand
     {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+
+        public abstract void Execute(object parameter);
+
     }
 }
